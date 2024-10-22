66daa78d3ac56f4869470da611d2765ec456a539
FlUxIuS <fluxius@gmail.com>
2024-10-23 00:46:25 +0200

    Adding crypto for payload decryption and encryption + list of weak keys + GRC examples dealing with hopping

---
+++ LoRa_PHYDecode-NG.py
@@ -0,0 +1,94 @@
+#!/usr/bin/en python
+
+from __future__ import print_function
+
+#    LoRa PHYdecoder - parse LoRa PHY decoded by gr-lora
+#    Copyright (C) 2020  Sebastien Dudek (@FlUxIuS) @Penthertz
+#    Code base on LoRa Craft project @PentHertz from commit a5f0a9d65c5ddc584035d5f45b52763e3a03a55f
+
+
+from layers import LoRa
+from scapy.layers.inet import UDP
+from scapy.sendrecv import sniff
+#from scapy.packet import bind_layers, Ether
+from scapy.all import *
+from scapy.utils import wrpcap
+from scapy.utils import rdpcap
+from lutil.crypto import *
+import argparse
+from lutil.fonts import *
+import code
+
+# default keys
+cur_NwSKey = "2B7E151628AED2A6ABF7158809CF4F3C"
+cur_AppSKey = "2B7E151628AED2A6ABF7158809CF4F3C"
+
+lpkt = b"" # last pkt
+
+savepcap = None
+
+def savePCAP(pkt):
+    global savepcap
+    wrpcap(savepcap, pkt, append=True)
+
+def decodePHY(pkt):
+    global lpkt, savepcap
+    if pkt != lpkt:
+        lpkt = pkt
+        decoded = LoRa(pkt[UDP].load)
+        direction = 1
+        print ()
+        if decoded.MType & 0b001 == 0b1:
+            direction = 0
+            print ("<"+"-"*30)
+        if decoded.MType & 0b001 == 0b0:
+            print ("-"*30+">")
+        print (repr(decoded))
+        if savepcap is not None:
+            savePCAP(pkt)
+
+        if cur_AppSKey is not None:
+            print (Fore.WHITE+Style.BRIGHT + "Deciphered Payload: ", decryptFRMPayload_1_0(binascii.unhexlify(cur_AppSKey), bytes(decoded), direction=direction), Style.RESET_ALL)
+
+
+def filterpkt(pkt, port):
+    if pkt.haslayer(UDP):
+        if pkt[UDP].dport == port and pkt != lpkt:
+            return True
+    return False
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                description='Monitor and decode MAC PHY packets.')
+    parser.add_argument('-p', '--port', dest='port', default=40868,
+                        help='TAP PORT to listen on (default: UDP 40868)')
+    parser.add_argument('-i', '--iface', dest='iface', default='lo',
+                        help='Interface to monitor (default: local)')
+    parser.add_argument('-v', '--version', dest='version', default='1.1',
+                        help='LoRaWAN version (1.1 by default)')
+    parser.add_argument('-o', '--output', dest='output', default=None,
+                        help='PCAP output filename')
+    parser.add_argument('-c', '--intercative', dest='interact', action='store_true',
+                        help='Interactive mode')
+    parser.add_argument('-n', '--NwSKey', dest='netskey', default=None,
+                        help='NwSKey')
+    parser.add_argument('-a', '--AppSKey', dest='appskey', default=None,
+                        help='AppSKey')
+
+    args = parser.parse_args()
+    iface = args.iface
+    port = int(args.port)
+
+    cur_NwSKey = args.netskey
+    cur_AppSKey = args.appskey
+
+    LoRa.version = args.version # setup LoRaWAN version
+    savepcap = args.output
+
+    if args.interact is True:
+        bind_layers(UDP, LoRa)
+        code.interact(local=locals())
+    else:
+        sniff(prn=decodePHY,
+            lfilter=lambda pkt: filterpkt(pkt, port),
+            iface=iface)
--- lutil/crypto.py
+++ lutil/crypto.py
@@ -66,4 +66,105 @@ def checkMIC(key, hexpkt, direction=1):
     print (repr(mic), repr(cmic))
     return (mic == cmic)
 
+
+def bruteforceJoinMIC(pkt , keylist, direction=1):
+    """
+        Bruteforce Join procedure AppKey/NwkKey
+            In(1): Bytes array of the packet
+            In(2): String path of the dictionnary list
+            In(3): Direction (1: network, 0: end device)
+    """
+    f = open(keylist, "r")
+    keys = f.readlines()
+    for key in keys:
+        print ("Testing: ", key)
+        key = binascii.unhexlify(key[:-1])
+        if checkMIC(key, pkt, direction) is True:
+            return ("Found AppKey/NwkKey: ", binascii.hexlify(key))
+
+
+def checkDATAMIC_10(key, hexpkt, direction=1):
+    """
+        Checks Data Payload MIC
+            In(1): Bytes array of CMAC key
+            In(2): Bytes array pf the packet
+            In(3): Direction (1: network, 0: end device)
+    """
+    direct = b"\x00" # uplink
+    lowoff = -6
+    mic = hexpkt[-6:-2]
+    if direction == 0: # don't skip the CRC
+        direct = b"\x01"
+        lowoff = -4
+        mic = hexpkt[-4:]
+    Fcnt = hexpkt[9:11]
+    b0 = b"\x49"+b"\x00"*4+direct+hexpkt[4:8]+Fcnt+b"\x00\x00"+b"\x00"+str.encode(chr(len(hexpkt[3:lowoff])))
+    cobj = CMAC.new(key, ciphermod=AES)
+    toret = cobj.update(b0+hexpkt[3:lowoff]).digest()
+    cmic = toret[:4]
+    return (mic == cmic)
+
+
+def checkDATAMIC_11(key, hexpkt, direction=1, DLACK=True):
+    # TODO: to check against real implem
+    """
+        Checks Data Payload MIC
+            In(1): Bytes array of CMAC key
+            In(2): Bytes array of the packet
+            In(3): Direction (1: network, 0: end device)
+            Int(4): Is the device connected to a network?
+    """
+    direct = b"\x00" # uplink
+    lowoff = -6
+    mic = hexpkt[-6:-2]
+    if direction == 0: # don't skip the CRC
+        direct = b"\x01"
+        lowoff = -4
+        mic = hexpkt[-4:]
+    Fcnt = hexpkt[9:11]
+    ConfFCnt = int.from_bytes(b"\x45\x34", "big") % 16
+    if DLACK is False: # if not confirmed uplink
+        ConfFCnt = "\x00"*2
+    b0 = b"\x49"+ ConfFCnt +b"\x00"*2+direct+hexpkt[4:8]+Fcnt+b"\x00\x00"+b"\x00"+str.encode(chr(len(hexpkt[3:lowoff])))
+    cobj = CMAC.new(key, ciphermod=AES)
+    toret = cobj.update(b0+hexpkt[3:lowoff]).digest()
+    cmic = toret[:4]
+    return (mic == cmic)
+
+
+def decryptFRMPayload(key, hexpkt, direction=1):
+    """
+        Decrypt the Payload
+            In(1): Bytes array of key used to encrypt the payload
+            In(2): Bytes array of the packet
+            In(3): Direction (1: network, 0: end device)
+    """
+    direct = b"\x00" # uplink
+    if direction == 0:
+        direct = b"\x01"
+    Fcnt = hexpkt[9:11]
+    DevAddr = hexpkt[4:8]
+    ai = lambda i : b"\x01"+b"\x00"*4+direct+DevAddr+Fcnt+b"\x00\x00"+b"\x00"+str.encode(chr(i))
+    FRM_Payload = pad(hexpkt[12:-4], 16) # must be aligned in block of 16 bytes
+    K = math.ceil(len(FRM_Payload)/16)
+    Si = lambda v_a : AES.new(key, AES.MODE_ECB).encrypt(v_a)
+    S = b''.join([Si(x) for x in [ai(y) for y in range(1, K+1)]])
+    pld_xor_S = bytes(a ^ b for (a, b) in zip(FRM_Payload, S))
+    return pld_xor_S
+
+
+def bruteforceDATAMIC_10(pkt , keylist, direction=1):
+    """
+        Decrypt the Payload
+            In(1): Bytes array of the packet
+            In(2): String path of the dictionnary list
+            In(3): Direction (1: network, 0: end device)
+    """
+    f = open(keylist, "r")
+    keys = f.readlines()
+    for key in keys:
+        print ("Testing: ", key)
+        key = binascii.unhexlify(key[:-1])
+        if checkDATAMIC_10(key, pkt, direction) is True:
+            return ("Found NwkSKey: ", binascii.hexlify(key))
 # TODO: more helpers
